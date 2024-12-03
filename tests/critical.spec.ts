9873600728a0a582b535f09f541c7a3e0219be44
ada-beeeee <ada.brewton@gmail.com>
2024-12-03 11:11:20 -0800

    Support testing local or prod with .env file

--- tests/critical.spec.ts
+++ tests/critical.spec.ts
@@ -1,7 +1,30 @@
 import { test, expect } from '@playwright/test';
 
+require('dotenv').config();
+
+function env(): string {
+  // Get test environment from environment variables
+  // (Default to dev environment if none specified at runtime)
+  if (process.env.TEST_ENV) {
+    return process.env.TEST_ENV;
+  } else {
+    return "local";
+  }
+}
+
+function home_url(): string {
+  if (env() == "production") {
+    console.log("Using production url https://incocollective.com/");
+    return 'https://incocollective.com/';
+  } else {
+    console.log("Using local url http://localhost:3000/");
+    return 'http://localhost:3000/';
+  }
+}
+
 test('InCo home page loads', async ({ page }) => {
-  await page.goto('https://incocollective.com/');
+
+  await page.goto(home_url());
 
   await expect(page).toHaveTitle(/Interconnected Collective/);
 
